e082b874587c4f787e7a04d23f4546038df0688d
misha <chemike47@gmail.com>
2018-03-23 11:38:00 +0300

    Implement commenter

---
+++ src/main/kotlin/org/lm/commenter/LmCommenter.kt
@@ -0,0 +1,16 @@
+package org.lm.commenter
+
+import com.intellij.lang.Commenter
+
+class LmCommenter : Commenter {
+
+    override fun getLineCommentPrefix(): String? = "#"
+
+    override fun getBlockCommentPrefix(): String? = null
+
+    override fun getBlockCommentSuffix(): String? = null
+
+    override fun getCommentedBlockCommentPrefix(): String? = null
+
+    override fun getCommentedBlockCommentSuffix(): String? = null
+}
