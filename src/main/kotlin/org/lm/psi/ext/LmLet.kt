97d6cd1a48fd0463e7904e664a6c44981d837b42
misha <chemike47@gmail.com>
2018-03-29 11:02:05 +0300

    Fix let

--- src/main/kotlin/org/lm/psi/ext/LmLet.kt
+++ src/main/kotlin/org/lm/psi/ext/LmLet.kt
@@ -2,14 +2,6 @@ package org.lm.psi.ext
 
 import com.intellij.lang.ASTNode
 import org.lm.psi.LmLet
-import org.lm.resolve.EmptyScope
-import org.lm.resolve.Scope
-import org.lm.resolve.namespace.EmptyNamespace
-import org.lm.resolve.namespace.Namespace
 
-abstract class LmLetImplMixin(node: ASTNode) : LmCompositeElementImpl(node), LmLet {
+abstract class LmLetImplMixin(node: ASTNode) : LmElementImpl(node), LmLet
 
-    override val namespace: Namespace = EmptyNamespace
-
-    override val scope: Scope = EmptyScope
-}
